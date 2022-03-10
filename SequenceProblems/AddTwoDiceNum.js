29709e72be64078423fe6390ee6e6bdd9fe96481
Rajeshwari2426 <rajarajeshwaribrgandi@gmail.com>
2022-03-10 23:09:31 +0530

    [Rajeshwari] adding Add two dice numbers

---
+++ SequenceProblems/AddTwoDiceNum.js
@@ -0,0 +1,5 @@
+//Two dice numbers
+let Dice1=Math.floor(Math.random()*7);
+let Dice2=Math.floor(Math.random()*7);
+console.log("Dice numbers : "+Dice1,+Dice2);
+console.log("Result : ",Dice1+Dice2);
