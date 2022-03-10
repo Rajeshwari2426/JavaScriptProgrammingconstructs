92d5a5164d8bfa59d666f5c933207ad350110752
Rajeshwari2426 <rajarajeshwaribrgandi@gmail.com>
2022-03-10 23:28:32 +0530

    [Rajeshwari] adding sum and avg of 5 num

---
+++ SequenceProblems/SumAndAvg.js
@@ -0,0 +1,14 @@
+let num1=Math.floor(Math.random()*89)+10;
+let num2=Math.floor(Math.random()*89)+10;
+let num3=Math.floor(Math.random()*89)+10;
+let num4=Math.floor(Math.random()*89)+10;
+let num5=Math.floor(Math.random()*89)+10;
+console.log("numbers:"+num1,+num2,+num3,+num4,+num5);
+// add  numbers
+const sum = num1 + num2 + num3 + num4 + num5;
+
+//average numbers
+const avg = (num1 + num2 + num3 + num4 + num5)/5;
+
+// display the sum and avg
+console.log(' The sum of: ' +sum+' Avg of: '+avg);
